14529b6833d62350dd01322ce9e8cfa4c7bf2233
Marc Bachofner <marc_bachofner@bluewin.ch>
2018-10-31 08:03:08 +0100

    dot product not finished

---
+++ maugiVektor.cpp
@@ -0,0 +1,19 @@
+#include <array>
+#include <iostream>
+
+int main() {
+	return 0;
+}
+
+int MDotProduct(int vec1 [], int vec2 []) {
+	int result;
+	
+	if(vec1.size() != vec2.size()) {
+		std::cout << "The vectors do not have the same dimension for dotproduct" << std::endl;
+		
+	}
+
+
+
+	return result;
+}
